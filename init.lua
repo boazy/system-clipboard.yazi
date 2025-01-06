f0c95396693fc473ff044988998ff08d87b83fed
Boaz Yaniv <boazyan@gmail.com>
2025-01-07 03:21:55 +0900

    Support paste action

--- init.lua
+++ init.lua
@@ -11,14 +11,27 @@ local selected_or_hovered = ya.sync(function()
 	return paths
 end)
 
-return {
-	entry = function()
+local notify = function(level, message)
+	return ya.notify({
+		title = "System Clipboard",
+		content = message,
+		level = level,
+		timeout = 5,
+	})
+end
+
+local notifyf = function(level, message, ...)
+	return notify(level, string.format(message, ...))
+end
+
+local commands = {
+	copy = function()
 		ya.manager_emit("escape", { visual = true })
 
 		local urls = selected_or_hovered()
 
 		if #urls == 0 then
-			return ya.notify({ title = "System Clipboard", content = "No file selected", level = "warn", timeout = 5 })
+			return notify("warn", "No file selected")
 		end
 
 		-- ya.notify({ title = #urls, content = table.concat(urls, " "), level = "info", timeout = 5 })
@@ -31,24 +44,82 @@ return {
 				:wait()
 
 		if status or status.succes then
-			ya.notify({
-				title = "System Clipboard",
-				content = "Succesfully copied the file(s) to system clipboard",
-				level = "info",
-				timeout = 5,
-			})
+			notifyf(
+				"info",
+				"Succesfully copied %s file(s) to system clipboard",
+				#urls
+			)
 		end
 
 		if not status or not status.success then
-			ya.notify({
-				title = "System Clipboard",
-				content = string.format(
-					"Could not copy selected file(s) %s",
-					status and status.code or err
-				),
-				level = "error",
-				timeout = 5,
-			})
+			notifyf(
+				"error",
+				"Could not copy selected file(s): %s",
+				status and status or err
+			)
 		end
+	end,
+
+	paste = function()
+		local info_output, err = Command("cb")
+				:arg("info"):output()
+
+		if err then
+			notify("error", "Error querying clipboard: " .. err)
+		end
+
+		local status = info_output.status
+		if not status or not status.success then
+			notifyf(
+				"error",
+				"Error querying clipboard: %s",
+				status and status or err
+			)
+		end
+
+		local json = require "util-json"
+		local ok, info = pcall(json.decode, info_output.stdout)
+		if not ok then
+			notifyf("error", "Error parsing clipboard info json: %s", info)
+		end
+
+		local files = info.files or 0
+
+		if files == 0 then
+			return notify("warn", "No file(s) in clipboard")
+		end
+
+		status, err =
+				Command("cb")
+				:arg("paste")
+				:env("CLIPBOARD_FORCETTY", "1")
+				:stdout(Command.NULL)
+				:spawn()
+				:wait()
+		if status and status.success then
+			notifyf(
+				"info",
+				"Succesfully pasted %s file(s) from system clipboard",
+				files
+			)
+		end
+		if not status or not status.success then
+			notifyf(
+				"error",
+				"Could not paste file(s) from system clipboard %s",
+				status and status or err
+			)
+		end
+	end,
+}
+
+return {
+	entry = function(_, job)
+		local cmd_name = job.args[1] or "copy"
+		local command = commands[cmd_name]
+		if not command then
+			return notifyf("error", "Invalid command name '%s'", cmd_name)
+		end
+		command()
 	end,
 }
